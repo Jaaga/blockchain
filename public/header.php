c50ecdadd8e10490e1d3c9284d8f5e3bf8b1001a
fatmazaman <fatma302@gmail.com>
2014-05-22 15:54:04 +0530

    header

--- public/header.php
+++ public/header.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html>
+
   <head>
-    <title>Header</title>
+        
+    <link rel="stylesheet" href="../dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../dist/buttons/css/buttons.css">
     <link rel="stylesheet" href="../dist/css/main.css">
+
   </head>
-
   <body>
-  	
+  <div class="container">
+    <div class="header">
+      <div class="pull-left">
+        <a href="#" class="button button-border-primary button-rounded" 
+        style="text-decoration:none;color: #00a1cb;" >Home</a>
+        <a href="#" class="button button-border-primary button-rounded" 
+        style="text-decoration:none;color: #00a1cb;" >About Us</a>
+        <a href="#" class="button button-border-primary button-rounded" 
+        style="text-decoration:none;color: #00a1cb;" > Contact</a>
+      </div>  
+      <div class="pull-right">
+        <a href="#" class="button button-border-primary button-rounded" 
+        style="text-decoration:none;color: #00a1cb;" >Sign In</a>
+        <a href="#" class="button button-border-primary button-rounded" 
+        style="text-decoration:none;color: #00a1cb;" >Sign Up</a>
+        <a href="#" class="button button-border-primary button-rounded" 
+        style="text-decoration:none;color: #00a1cb;" > Help</a>
+        </div> 
+    </div>
+  </div>    
 
 
 
+  </body>
 
 
-  </body> 
-</html>  
+</html>
