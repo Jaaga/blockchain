d862277025bd70113880118b7e5cdf5c1fa6eacf
fatmazaman <fatma302@gmail.com>
2014-05-22 10:56:19 +0530

    create_DB

---
+++ public/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Header</title>
+    <link rel="stylesheet" href="../dist/css/main.css">
+  </head>
+
+  <body>
+  	
+
+
+
+
+
+  </body> 
+</html>  
---
+++ public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+        
+    <link rel="stylesheet" href="../dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../dist/buttons/css/buttons.css">
+    <link rel="stylesheet" href="../dist/css/main.css">
+
+  </head>
+  <body>
+  <div class="container">
+    <div class="header">
+      <div class="pull-left">
+        <a href="#" class="button button-border-primary button-rounded" 
+        style="text-decoration:none;color: #00a1cb;" >Home</a>
+        <a href="#" class="button button-border">Home</a>
+        <a href="#" class="button button-border">About Us</a>
+        <a href="#" class="button button-border"> Contact</a>
+      </div>  
+      <div class="pull-right">
+        <a href="#" class="button button-border">Sign In</a>
+        <a href="#" class="button button-border">Sign Up</a>
+        <a href="#" class="button button-border"> Help</a>
+      </div> 
+    </div>
+  </div>    
+
+
+
+  </body>
+
+
+</html>
