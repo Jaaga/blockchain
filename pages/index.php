b6674a16d08e5b86a544a33a0137716bc52c2838
peter pits <thengugi@gmail.com>
2014-06-26 01:02:36 +0530

    pages

---
+++ pages/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Blockchain</title>
+		<meta name="viewport, width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="../public/css/normalise.css" />
+		<link rel="stylesheet" type="text/css" href="../public/css/main.css" />
+		<link rel="stylesheet" type="text/css" href="../public/css/resposnive.css">
+		<link href='http://fonts.googleapis.com/css?family=Dosis:300' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+
+	</head>
+
+	<header>
+
+		<div id="logo">
+			<a href="index.php">
+				<h1>Blockchain</h1>
+				<h2>Blockchain allows your business to start accepting bitcoin instantly, 
+				securely, and for free.</h2>
+			</a>
+		</div>
+
+		<div id="nav-container">
+			<nav>
+				<ul>
+					<li><a href="index.php" class="selected">Blockchain</a></li>
+					<li><a href="#">How To</a></li>
+					<li><a href="#">Chart</a></li>
+					<li><a href="#">Trending</a></li>
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+			</nav>
+		</div>
+
+	</header>
+
+	<body>
+		<div id="wrapper">
+			<section>
+				
+			</section>
+
+				<aside>
+					<div id="floating-menu">
+						<ul class="menu1">
+							<li><a href="index.php" onclick="return false;">Home</a></li>
+						</ul>
+
+						<ul class="menu2">
+							<li><a href="#" onclick="return false;">Buy</a></li>
+							<li><a href="#" onclick="return false;">Sell</a></li>
+						</ul>
+
+						<ul class="menu3">
+							<li><a href="#" onclick="return false;">Exchanges</a></li>
+							<li><a href="#" onclick="return false;">Merchants</a></li>
+							<li><a href="#" onclick="return false;">Payment</a></li>
+						</ul>
+					</div>
+				</aside>
+
+	</div>
+
+	<div id="nav-container">
+		<footer>
+			<p>&copy; 2014 All Rights Reserved</p>
+			<a href="#"><img src="../public/images/facebook_icon.png" alt="facebook-icon" class="social_icon"></a>
+			<a href="#"><img src="../public/images/Twitter_icon" alt="twitter-icon" class="social_icon"></a>
+			<a href="#"><img src="../public/images/google-icon.png" alt="google-icon" class="social_icon"></a>				
+		</footer>
+	</div>
+
+	</body>
+
+</html>
